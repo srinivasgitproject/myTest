f9720aa583ce0c007b924c56fa639b5e6973d352
srinivasgitproject <59089325+srinivasgitproject@users.noreply.github.com>
2019-12-20 19:01:03 +0530

    Add files via upload

---
+++ LogTest.java
@@ -0,0 +1,25 @@
+package test.com;
+
+import java.io.*;
+import java.sql.SQLException;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+
+public class LogTest {
+
+	
+	/* Get the class name to be printed on */  
+	   //static Logger log = Logger.getLogger(LogTest.class.getName());  
+	     
+	   public static void main(String[] args)throws IOException,SQLException{  
+		  /* PropertyConfigurator.configure("application.properties");
+		   log.debug("Hello this is a debug message");  
+	      log.info("Hello this is an info message");  
+	      System.out.println("Hi..Cool");
+	      URLTest u=new URLTest();
+	      u.sendGET();*/
+		  System.out.println("Hi..IMI");
+	   }  
+}
