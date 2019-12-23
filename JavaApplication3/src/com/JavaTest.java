9d7d7531e1637f5b4c9eb5f6c1ce585de90842dd
srinivasgitproject <59089325+srinivasgitproject@users.noreply.github.com>
2019-12-23 20:26:20 +0530

    Update JavaTest.java

--- JavaApplication3/src/com/JavaTest.java
+++ JavaApplication3/src/com/JavaTest.java
@@ -16,7 +16,7 @@ public class JavaTest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello...........$$$$$$$$$$$@@@@@@@@@@#####App");
+        System.out.println("Hello...........$$$$$$$$$$$@@@@@@@@@@#####----Application started");
     }
     
 }
